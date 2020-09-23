1525e5452388f9411c9cdb742f3d747b6f200ae5
Gunther Laure <gunther.laure@dewetron.com>
2020-09-23 13:37:43 +0200

    Version bump to 1.2

--- app_update/version_info/MajorVersion.h
+++ app_update/version_info/MajorVersion.h
@@ -2,7 +2,7 @@
 //to be manually edited!
 
 #define  VERSION_MAJOR       1
-#define  VERSION_MINOR       1
+#define  VERSION_MINOR       2
 #define  VERSION_MICRO       0
 
 #define FILE_DESCRIPTION_STRING "AppUpdate"
